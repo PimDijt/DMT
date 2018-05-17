a316216a003f154e84d9bd0dcc75df8a3be7a603
Pim Dijt <pimdijt@gmail.com>
2018-05-17 20:00:03 +0200

    updated get_data script

--- assignments/assignment2/get_data.sh
+++ assignments/assignment2/get_data.sh
@@ -3,5 +3,3 @@ head -n 100000 data/training_set_VU_DM_2014.csv > data/training_100K.csv &&
 head -n 250000 data/training_set_VU_DM_2014.csv > data/training_250K.csv &&
 head -n 500000 data/training_set_VU_DM_2014.csv > data/training_500K.csv &&
 head -n 1000000 data/training_set_VU_DM_2014.csv > data/training_1M.csv
-~
-
