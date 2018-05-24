e5fdc177f1d45c5937b0a1ee744ab58a4779c99c
Pim Dijt <pimdijt@gmail.com>
2018-05-24 11:38:48 +0200

    new results

--- assignments/assignment2/combine_data.sh
+++ assignments/assignment2/combine_data.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cat data/training_100K.csv data/training_100K_T.csv > data/training_200K_C.csv &&
-cat data/training_100K_T.csv data/training_100K.csv > data/trianing_200K_CR.csv
+head -n 1 data/training_100K.csv >> data/training_200K_CR.csv &&
+cat data/training_100K_T.csv >> data/training_200K_CR.csv &&
+tail -n +2 data/training_100K.csv >> data/training_200K_CR.csv
